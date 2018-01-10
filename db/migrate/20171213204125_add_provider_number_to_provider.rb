4ce2ae7d6608d492c3df6336b96909d89e6375db
Rob Schwartz <rob.schwartz@vibes.com>
2018-01-10 10:47:46 -0500

    removed some migrations

--- db/migrate/20171213204125_add_provider_number_to_provider.rb
+++
@@ -1,5 +0,0 @@
-class AddProviderNumberToProvider < ActiveRecord::Migration[5.1]
-  def change
-    add_column :providers, :provider_number, :string
-  end
-end
--- db/migrate/20171213210331_add_medicaid_to_provider.rb
+++
@@ -1,6 +0,0 @@
-class AddMedicaidToProvider < ActiveRecord::Migration[5.1]
-  def change
-    add_column :providers, :medicaid, :boolean
-    remove_column :providers, :medicade
-  end
-end
